b39aa01050ca8bb7b624134e0b9ec594c19e56ac
Emma Buller <dib5017@gmail.com>
2021-02-11 13:17:36 -0500

    toString appears to work for empty boards

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -3,4 +3,20 @@ public class QueenBoard {
   public QueenBoard(int size) {
     board = new int[size][size];
   }
+
+  public String toString() {
+    String stringBoard = "";
+    for(int i = 0; i<board.length; i++) {
+      for(int j = 0; j<board.length; j++) {
+        if(board[i][j]>=0) {
+          stringBoard = stringBoard + "_ ";
+        }
+        else {
+          stringBoard = stringBoard + "Q ";
+        }
+      }
+      stringBoard = stringBoard + "\n";
+    }
+    return stringBoard;
+  }
 }
