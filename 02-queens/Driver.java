b9a7e7dc4b83e17494ccb7f3f5f8a8e20ff883b1
Emma Buller <dib5017@gmail.com>
2021-02-19 19:46:03 -0500

    Wrote addQueen method and doing some testing

--- 02-queens/Driver.java
+++ 02-queens/Driver.java
@@ -2,5 +2,11 @@ public class Driver {
   public static void main(String[] args) {
     QueenBoard q = new QueenBoard(8);
     System.out.println(q.toString());
+
+    q.addQueen(0,0);
+    System.out.println(q.toString());
+    System.out.println(q.addQueen(5,0));
+    System.out.println(q.addQueen(2,2));
+    System.out.println(q.addQueen(0,6));
   }
 }
--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -9,14 +9,53 @@ public class QueenBoard {
     for(int i = 0; i<board.length; i++) {
       for(int j = 0; j<board.length; j++) {
         if(board[i][j]>=0) {
+          //is it a queen;
           stringBoard = stringBoard + "_ ";
         }
         else {
+          //if so do this;
           stringBoard = stringBoard + "Q ";
         }
       }
       stringBoard = stringBoard + "\n";
     }
     return stringBoard;
+  }
+
+  public boolean addQueen(int r, int c) {
+    //check if queen is already there OR if it's a dangerous spot
+    if(board[r][c]<=0) {
+      board[r][c] = -1; //queen value;
+      for(int i = 1; i<board.length; i++) {
+        int rAdd = r+i;
+        int rSub = r-i;
+        int cAdd = c+i;
+        int cSub = c-i;
+        if(rAdd<board.length) {
+          board[rAdd][c] = 1;
+          if(cAdd<board.length) {
+            board[r][cAdd] = 1;
+            board[rAdd][cAdd] = 1;
+          }
+          if(cSub>=0) {
+            board[r][cSub] = 1;
+            board[rAdd][cSub] = 1;
+          }
+        }
+        if(rSub>=0) {
+          board[rSub][c] = 1;
+          if(cAdd<board.length) {
+            board[r][cAdd] = 1;
+            board[rSub][cAdd] = 1;
+          }
+          if(cSub>=0) {
+            board[r][cSub] = 1;
+            board[rSub][cSub] = 1;
+          }
+        }
+      }
+      return true;
+    }
+    return false;
   }
 }
