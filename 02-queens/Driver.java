be15828cafc4f924b105bf30e52308cb6d2884a3
Emma Buller <dib5017@gmail.com>
2021-02-19 19:48:48 -0500

    Did some more testing, Will change add back to private once i tested remove

--- 02-queens/Driver.java
+++ 02-queens/Driver.java
@@ -8,5 +8,10 @@ public class Driver {
     System.out.println(q.addQueen(5,0));
     System.out.println(q.addQueen(2,2));
     System.out.println(q.addQueen(0,6));
+    q.addQueen(5,7);
+    System.out.println(q.toString());
+    q.addQueen(6,4);
+    System.out.println(q.toString());
+    System.out.println(q.addQueen(5,4));
   }
 }
