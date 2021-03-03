cb6cbaf78f0d98a7619ea721a5e94259e1f5e4af
Emma Buller <dib5017@gmail.com>
2021-03-02 22:01:57 -0500

    isBorder is working I think?

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -4,13 +4,35 @@ public class MazeGenerator {
   public static void generate(char[][]maze,int startrow, int startcol) {
     maze[startrow][startcol] = ' ';
     ArrayList<String> direction = new ArrayList<String>(Arrays.asList(new String[]{"N","S","W","E"}));
-    
+    for(int i = 0; i<4; i++) {
+      int newRow = startrow;
+      int newCol = startcol;
+      Random ran = new Random();
+      int index = ran.nextInt(direction.size());
+      String d = direction.get(index);
+      if(d.equals("N")) {
+        newRow+=1;
+      }
+      else if(d.equals("S")) {
+        newRow-=1;
+      }
+      else if(d.equals("E")) {
+        newCol+=1;
+      }
+      else {
+        newCol-=1;
+      }
+      direction.remove(index);
+      if(isBorder(maze,newRow,newCol)) {
+        System.out.println(newRow + " " + newCol);
+      }
+    }
   }
-  private boolean isBorder(char[][] maze, int row, int col) {
-    return (row!=0 && row!=maze.length) && (col!=0 && row!=maze.length);
+  public static boolean isBorder(char[][] maze, int row, int col) {
+    return (row>0 && row<maze.length-1) && (col>0 && col<maze[0].length-1);
    }
 
-   private boolean safeSquare(char[][] maze, int row, int col) {
+   private static boolean safeSquare(char[][] maze, int row, int col) {
      int counter = 0;
      if(maze[row+1][col]==' ') {
        counter+=1;
---
+++ 03-maze/generatorDriver.java
@@ -0,0 +1,12 @@
+public class generatorDriver {
+  public static void main(String[] args) {
+    char[][] maze = new char[10][10];
+    for(int i = 0; i<maze.length; i++) {
+      for(int j = 0; j<maze[0].length; j++) {
+        maze[i][j] = '#';
+      }
+    }
+    //System.out.println(MazeGenerator.isBorder(maze,10,10))
+    MazeGenerator.generate(maze,1,1);
+  }
+}
