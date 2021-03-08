1f3caa8371c8bc342d87c8cd75bb3b3355f5c1d1
Emma Buller <dib5017@gmail.com>
2021-03-07 20:21:35 -0500

    Made the method that does the steps of the bronze method, I didn't parse through the file yet

--- 04-usaco/Tester.java
+++ 04-usaco/Tester.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class Tester {
   public static void main(String[] args) {
     int[][] lake = {{28,25,20,32,34,36},{27,25,20,20,30,34},{24,20,20,20,20,30},{20,20,14,14,20,20}};
-    USACO.stompDig(lake,1,4,4);
-    USACO.stompDig(lake,1,1,10);
+    int[][] steps = {{1,4,4},{1,1,10}};
+    // USACO.stompDig(lake,1,4,4);
+    // USACO.stompDig(lake,1,1,10);
+    System.out.println(USACO.bronze(lake,steps,22));
     System.out.println(Arrays.deepToString(lake));
   }
 }
--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -1,4 +1,23 @@
 public class USACO {
+  public static int bronze(int[][] elevations, int[][] steps, int average) {
+    int sum = 0;
+    for(int s = 0; s<steps.length; s++) {
+      stompDig(elevations, steps[s][0], steps[s][1], steps[s][2]);
+    }
+    for(int r = 0; r<elevations.length; r++) {
+      for(int c = 0; c<elevations[0].length; c++) {
+        if(average-elevations[r][c]>0) {
+          elevations[r][c] = average-elevations[r][c];
+        }
+        else {
+          elevations[r][c] = 0;
+        }
+        sum+=elevations[r][c];
+      }
+    }
+    return sum*72*72;
+  }
+
   public static void stompDig(int[][] elevations, int row, int col, int dig) {
     int max = 0;
     for(int i = row-1; i<row+2; i++) {
