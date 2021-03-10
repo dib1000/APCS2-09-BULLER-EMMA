eacd2c46348d513d26efe008aad5c786ad81c36c
Emma Buller <dib5017@gmail.com>
2021-03-10 16:26:08 -0500

    I think the swapping method not including equal values and the last swap works

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -6,6 +6,21 @@ public class Preliminary {
     data[start] = data[random];
     data[random] = storage;
     System.out.println(Arrays.toString(data));
+    int current = start+1;
+    int greater = end;
+    while(current<greater) {
+      if(data[current]<data[start]) {
+        current+=1;
+      }
+      else if(data[current]>data[start]) {
+        int swap = data[greater];
+        data[greater] = data[current];
+        data[current] = swap;
+        greater-=1;
+      }
+    }
+    System.out.println(current + "," + greater);
+    System.out.println(Arrays.toString(data));
     return 35;
    }
 }
