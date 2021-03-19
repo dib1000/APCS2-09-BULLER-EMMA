d9d9635836311b848f9e0b96324d50aad1b7957f
Emma Buller <dib5017@gmail.com>
2021-03-18 23:16:51 -0400

    Made merge method

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -4,31 +4,47 @@ public class Merge {
     //loop thing i don't know yet
     //calls merge(int[] data, int lo, int hi)
   }
-  public static void merge(int[] data, int lo, int hi) {
-    int median = (hi-lo)/2;
-    if((hi-lo)%2==0) {
-      int[] a = new int[median];
-      int[] b = new int[median];
-      for(int i = lo; i<lo+median; i++) {
-        a[i-lo] = data[i];
+  public static void merge(int[] data, int[] temp, int lo, int hi) {
+      int median = (hi-lo)/2;
+      if(median>0) {
+        if((hi-lo)%2!=0) {
+          median++;
+        }
+        int ex = median+lo;
+        int start = lo;
+        int index = lo;
+        while(index<hi) {
+          if(start>=(median+lo) || ex>=hi) {
+            if(start>=(median+lo)) {
+              temp[index] = data[ex];
+              index++;
+              ex++;
+            }
+            else{
+              temp[index] = data[start];
+              index++;
+              start++;
+            }
+        }
+        else {
+          if(data[start]<data[ex]) {
+            temp[index] = data[start];
+            index++;
+            start++;
+          }
+          else {
+            temp[index] = data[ex];
+            index++;
+            ex++;
+          }
+        }
+        System.out.println(Arrays.toString(temp));
+        System.out.println(start+" "+ex+" "+index);
+        //System.out.println(data[start]+" "+data[ex]);
       }
-      for(int j = (lo+median); j<hi; j++) {
-        b[j-(lo+median)] = data[j];
-      }
-      System.out.println(Arrays.toString(a));
-      System.out.println(Arrays.toString(b));
     }
     else {
-      int[] a = new int[median+1];
-      int[] b = new int[median];
-      for(int i = lo; i<lo+median+1; i++) {
-        a[i-lo] = data[i];
-      }
-      for(int j = (lo+median+1); j<hi; j++) {
-        b[j-(lo+median+1)] = data[j];
-      }
-      System.out.println(Arrays.toString(a));
-      System.out.println(Arrays.toString(b));
+      temp[lo] = data[lo];
     }
   }
 }
--- 06-merge/Tester.java
+++ 06-merge/Tester.java
@@ -1,6 +1,15 @@
+import java.util.*;
 public class Tester {
   public static void main(String[] args) {
     int[] x = {38,27,43,3,9,82,10};
-    Merge.merge(x,4,7);
+    int[] temp = new int[7];
+    Merge.merge(x,temp,0,2);
+    Merge.merge(x,temp,2,4);
+    Merge.merge(x,temp,4,6);
+    Merge.merge(x,temp,6,7);
+    Merge.merge(temp,x,0,4);
+    Merge.merge(temp,x,4,7);
+    Merge.merge(x,temp,0,7);
+    System.out.println(Arrays.toString(temp));
   }
 }
