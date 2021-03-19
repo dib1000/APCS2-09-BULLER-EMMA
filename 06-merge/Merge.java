0cbfc2e411a577c0a2143e67af929b93f12d65dd
Emma Buller <dib5017@gmail.com>
2021-03-19 00:54:39 -0400

    Currently have a no recursion solution for mergesort that is a bit flawed

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,8 +1,42 @@
 import java.util.*;
 public class Merge {
   public static void mergesort(int[] data) {
-    //loop thing i don't know yet
-    //calls merge(int[] data, int lo, int hi)
+    int[] temp = new int[data.length];
+    int pair = 2;
+    int swaps = 1;
+    int howMany = ((int)(Math.log(data.length)/Math.log(2)))+1;
+    while((Math.log(pair)/Math.log(2))<=howMany) {
+      // System.out.println(pair);
+      // System.out.println(swaps);
+      if(swaps%2!=0) {
+        for(int i = 0; i<data.length; i+=pair) {
+          if(i+pair<data.length) {
+            merge(data,temp,i,i+pair);
+          }
+          else {
+            //System.out.println("WE DID IT");
+            merge(data,temp,i,data.length);
+          }
+        }
+      }
+      else {
+        for(int i = 0; i<data.length; i+=pair) {
+          if(i+pair<data.length) {
+            merge(temp,data,i,i+pair);
+          }
+          else {
+            merge(temp,data,i,data.length);
+          }
+        }
+      }
+      swaps++;
+      pair = pair * 2;
+    }
+    if(swaps%2==0) {
+      for(int i = 0; i<data.length; i++) {
+        data[i] = temp[i];
+      }
+    }
   }
   public static void merge(int[] data, int[] temp, int lo, int hi) {
       int median = (hi-lo)/2;
@@ -38,8 +72,8 @@ public class Merge {
             ex++;
           }
         }
-        // System.out.println(Arrays.toString(temp));
-        // System.out.println(start+" "+ex+" "+index);
+        System.out.println(Arrays.toString(temp));
+        System.out.println(start+" "+ex+" "+index);
         //System.out.println(data[start]+" "+data[ex]);
       }
     }
--- 06-merge/Tester.java
+++ 06-merge/Tester.java
@@ -3,13 +3,21 @@ public class Tester {
   public static void main(String[] args) {
     int[] x = {38,27,43,3,9,82,10};
     int[] temp = new int[7];
-    Merge.merge(x,temp,0,2);
-    Merge.merge(x,temp,2,4);
-    Merge.merge(x,temp,4,6);
-    Merge.merge(x,temp,6,7);
-    Merge.merge(temp,x,0,4);
-    Merge.merge(temp,x,4,7);
-    Merge.merge(x,temp,0,7);
-    System.out.println(Arrays.toString(temp));
+    // Merge.merge(x,temp,0,2);
+    // Merge.merge(x,temp,2,4);
+    // Merge.merge(x,temp,4,6);
+    // Merge.merge(x,temp,6,7);
+    // Merge.merge(temp,x,0,4);
+    // Merge.merge(temp,x,4,7);
+    // Merge.merge(x,temp,0,7);
+     Merge.mergesort(x);
+    System.out.println(Arrays.toString(x));
+    // int[] equal = {1,1,1,1};
+    // int[] t = new int[4];
+    // Merge.merge(equal,t,0,2);
+    // System.out.println(Arrays.toString(t));
+    int[] reverse = {10,9,8,7,6,5,4,3,2,1};
+    Merge.mergesort(reverse);
+    System.out.println(Arrays.toString(reverse));
   }
 }
