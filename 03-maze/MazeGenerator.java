163910e3fc8c66e54e545b19abde0aa033d3b715
Emma Buller <dib5017@gmail.com>
2021-03-19 01:42:02 -0400

    Changed the errors to mazegenerator

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -2,30 +2,32 @@ import java.util.*;
 
 public class MazeGenerator {
   public static void generate(char[][]maze,int startrow,int startcol) {
-    maze[startrow][startcol] = ' ';
-    ArrayList<String> direction = new ArrayList<String>(Arrays.asList(new String[]{"N","S","W","E"}));
-    for(int i = 0; i<4; i++) {
-      int newRow = startrow;
-      int newCol = startcol;
-      Random ran = new Random();
-      int index = ran.nextInt(direction.size());
-      String d = direction.get(index);
-      if(d.equals("N")) {
-        newRow+=1;
-      }
-      else if(d.equals("S")) {
-        newRow-=1;
-      }
-      else if(d.equals("E")) {
-        newCol+=1;
-      }
-      else {
-        newCol-=1;
-      }
-      direction.remove(index);
-      if(isBorder(maze,newRow,newCol)&& maze[newRow][newCol]!=' ') {
-        if(safeSquare(maze,newRow,newCol)) {
-          generate(maze,newRow,newCol);
+    if(maze[0].length>2 && maze.length>2 && isBorder(maze,startrow,startcol)) {
+      maze[startrow][startcol] = ' ';
+      ArrayList<String> direction = new ArrayList<String>(Arrays.asList(new String[]{"N","S","W","E"}));
+      for(int i = 0; i<4; i++) {
+        int newRow = startrow;
+        int newCol = startcol;
+        Random ran = new Random();
+        int index = ran.nextInt(direction.size());
+        String d = direction.get(index);
+        if(d.equals("N")) {
+          newRow+=1;
+        }
+        else if(d.equals("S")) {
+          newRow-=1;
+        }
+        else if(d.equals("E")) {
+          newCol+=1;
+        }
+        else {
+          newCol-=1;
+        }
+        direction.remove(index);
+        if(isBorder(maze,newRow,newCol)&& maze[newRow][newCol]!=' ') {
+          if(safeSquare(maze,newRow,newCol)) {
+            generate(maze,newRow,newCol);
+          }
         }
       }
     }
--- 03-maze/generatorDriver.java
+++ 03-maze/generatorDriver.java
@@ -1,14 +1,14 @@
 
 public class generatorDriver {
   public static void main(String[] args) {
-    char[][] maze = new char[10][75];
+    char[][] maze = new char[5][5];
     for(int i = 0; i<maze.length; i++) {
       for(int j = 0; j<maze[0].length; j++) {
         maze[i][j] = '#';
       }
     }
     //System.out.println(MazeGenerator.isBorder(maze,10,10))
-    MazeGenerator.generate(maze,5,43);
+    MazeGenerator.generate(maze,5,5);
     System.out.println(MazeGenerator.toString(maze));
   }
 }
