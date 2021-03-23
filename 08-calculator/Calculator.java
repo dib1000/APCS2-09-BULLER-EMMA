a6f35e8ec7da10756864ec8efd957564bc106dd1
Emma Buller <dib5017@gmail.com>
2021-03-23 13:14:24 -0400

    Split the actions of the method depending on if the String is an operator or an operand

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,10 +1,18 @@
 import java.util.*;
 public class Calculator {
   public static double eval(String s) {
-    String[] operators = s.split(" ");
+    String[] values = s.split(" ");
     ArrayDeque vals = new ArrayDeque();
-    for(int i = 0; i<operators.length; i++) {
-      System.out.println(operators[i]);
+    ArrayList<String> operators = new ArrayList<String>(Arrays.asList(new String[] {"+","-","*","/","%"}));
+    for(int i = 0; i<values.length; i++) {
+      if(operators.contains(values[i])) {
+        System.out.println("Operation: " + values[i]);
+      }
+      else {
+        double convert = Double.parseDouble(values[i]);
+        vals.addLast(convert);
+        System.out.println("Operand: "+vals.getLast());
+      }
     }
     return 0.0;
   }
