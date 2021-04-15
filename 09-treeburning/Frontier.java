473aea89d46495031d254874708e16ce94081549
Emma Buller <dib5017@gmail.com>
2021-04-15 15:48:26 -0400

    Started making constructor for frontier

---
+++ 09-treeburning/Frontier.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Frontier {
+  private ArrayDeque frontier;
+
+  public Frontier() {
+    frontier = new ArrayDeque(1000);
+  }
+}
---
+++ 09-treeburning/frontierTester.java
@@ -0,0 +1,5 @@
+public class frontierTester {
+  public static void main(String[] args) {
+    Frontier x = new Frontier();
+  }
+}
