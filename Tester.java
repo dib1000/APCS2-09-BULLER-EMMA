28d8a5dbcf2cb62813411267189da1f7e8119761
Emma Buller <dib5017@gmail.com>
2021-04-25 17:24:35 -0400

    Did some more assorted testing with pushDown

--- Tester.java
+++ Tester.java
@@ -4,6 +4,22 @@ public class Tester {
   public static void main(String[] args) {
     int[] nums = {8, 16, 9, 13, 4, 0, 11};
     MyHeap.pushDown(nums,7,0);
+    MyHeap.pushDown(nums,7,6);
+    MyHeap.pushDown(nums,7,2);
     System.out.println(Arrays.toString(nums));
+
+    int[] fill = new int[10];
+    for(int i = 0; i<6; i++) {
+      fill[i] = (int)(Math.random() * 100);
+    }
+    System.out.println(Arrays.toString(fill));
+    MyHeap.pushDown(fill,6,0);
+    System.out.println(Arrays.toString(fill));
+    MyHeap.pushDown(fill,6,2);
+    System.out.println(Arrays.toString(fill));
+
+    int[] child = {9,16};
+    MyHeap.pushDown(child,2,0);
+    System.out.println(Arrays.toString(child));
   }
 }
